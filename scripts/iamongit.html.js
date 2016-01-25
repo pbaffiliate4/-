d851b9ae5b32edde5526d8c601d470a8572cc87d
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-01-25 15:53:23 +0200

    Update iamongit.html.js

--- scripts/iamongit.html.js
+++ scripts/iamongit.html.js
@@ -4,4 +4,4 @@ write=nobody
 execute=authenticated 
   **/ 
  /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>bold</bold>"},"scriptrdata":[]}}*#*#*/
-var content= '<b>bold</bold>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
+var content= '<b>BrunA</bold>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
