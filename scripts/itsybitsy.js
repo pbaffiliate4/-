dfffb62fec750bc1b7c740b081f5931c92306399
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-01-25 16:39:12 +0200

    Update itsybitsy.js

--- scripts/itsybitsy.js
+++ scripts/itsybitsy.js
@@ -4,4 +4,4 @@ write=nobody
 execute=authenticated 
   **/ 
  /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<b>spider</b>"},"scriptrdata":[]}}*#*#*/
-var content= '<b>spider</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
+var content= '<b>see saw up and down</b>'; var response = apsdb.httpRespond(); response.write(content);response.close();			
